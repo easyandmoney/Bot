1e68ee0cc6d181d01f040abefaa4236ff62e6c9d
Nastya <annika-purtova@yandex.ru>
2023-01-14 11:52:43 +0300

    Updates

--- bot/clients/operations.py
+++ bot/clients/operations.py
@@ -1,20 +1,29 @@
-import requests
-
 from datetime import datetime
+
+import requests
+import orjson
 
 
 class OperationsClient:
     def __init__(self, url: str) -> None:
         self.url = url
-    def add(self, user_id: int, category: str, amount: int, is_income: bool, payment_date: datetime):
+
+    def add(
+        self,
+        user_id: int,
+        category: str,
+        amount: int,
+        is_income: bool,
+        payment_date: datetime,
+    ):
         url = f'{self.url}/api/v1/users/{user_id}/operations/'
         payload = {
             'name': category,
             'amount': amount,
             'type_income_expenses': 'income' if is_income else 'expense',
-            'payment_date': payment_date
+            'payment_date': payment_date,
         }
         headers = {'Content-Type': 'application/json'}
-        response = requests.request('POST', url, json=payload, headers=headers)
+        response = requests.request('POST', url, data=orjson.dumps(payload), headers=headers)
         response.raise_for_status()
         return response.json()
